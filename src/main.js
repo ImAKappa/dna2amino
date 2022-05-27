bb60eed22554063de1308191b83c1dafc809809c
ImAKappa <imaninconsp1cuouskappa@gmail.com>
2022-05-26 20:52:31 -0400

    Convert to Svelte

--- src/main.js
+++ src/main.js
@@ -1,36 +1,10 @@
-// Instantiate Classes
-let geneCode = new GeneticCode();
-let translationMachinery = new TranslationMachines(geneCode.aminoCodonDict);
+import App from './App.svelte';
 
-// Input Handling //
-
-let inputBox = document.querySelector('#input-codons');
-
-let butt = document.querySelector('#start-btn');
-
-let clearButt = document.querySelector('#clear-btn');
-
-let inputHandler = new InputHandler(inputBox, butt, clearButt);
-
-let aBtn = document.querySelector('#a-btn').addEventListener('click', () => {
-  inputBox.value += 'A';
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'world'
+	}
 });
 
-let tBtn = document.querySelector('#t-btn').addEventListener('click', () => {
-  inputBox.value += 'T';
-});
-
-let cBtn = document.querySelector('#c-btn').addEventListener('click', () => {
-  inputBox.value += 'C';
-});
-
-let gBtn = document.querySelector('#g-btn').addEventListener('click', () => {
-  inputBox.value += 'G';
-});
-
-let spaceBtn = document.querySelector('#space-btn').addEventListener('click', () => {
-  inputBox.value += ' ';
-});
-
-// Other Output things
-
+export default app;
